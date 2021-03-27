b1f18352c269cf82d99c3016458c6ee167e854f8
dnesov <d.nesov@gmail.com>
2021-03-27 19:56:22 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,115 @@
+#![allow(dead_code)]
+
+extern crate rand;
+use rand::Rng;
+
+const NAMES: [&'static str; 15] = [
+    "John", "Daniel", "Mark", "Jonathan", "Jason", "Michael", "Madeline", "Rudolf", "Lester",
+    "Shawn", "Tom", "Adrian", "Kevin", "Ralph", "Connor",
+];
+
+const SURNAMES: [&'static str; 16] = [
+    "Peck",
+    "Mcconnell",
+    "Kelley",
+    "Macias",
+    "Welch",
+    "Hurley",
+    "Hartman",
+    "Dorsey",
+    "Villegas",
+    "Ochoa",
+    "Hamilton",
+    "Robinson",
+    "Gray",
+    "Greenwood",
+    "Robinson",
+    "Walker",
+];
+
+
+pub struct Student {
+    name: String,
+    class_id: String,
+    marks: Vec<i8>,
+}
+
+impl Student {
+    pub fn new(name: String, class_id: String, marks: Vec<i8>) -> Self {
+        return Student {
+            name: name,
+            class_id: class_id,
+            marks: marks,
+        };
+    }
+    fn pretty_print(&self) {
+        println!(
+            "Student's name: {}\nStudent's class: {}\nStudent's average: {}",
+            self.name,
+            self.class_id,
+            self.average_mark()
+        );
+    }
+    fn average_mark(&self) -> f32 {
+        let avg: f32;
+
+        let total: i8 = self.marks.iter().sum();
+        avg = total as f32 / self.marks.len() as f32;
+
+        return avg;
+    }
+}
+
+pub struct School {
+    name: String,
+    students: Vec<Student>,
+}
+
+impl School {
+    pub fn new(name: String, students: Vec<Student>) -> School {
+        return School {
+            name: name,
+            students: students,
+        };
+    }
+
+    fn generate_students(&self, amount: i32) -> Vec<Student> {
+        let mut students: Vec<Student> = vec![];
+
+        let mut rng = rand::thread_rng();
+
+        for _x in 1..amount {
+            let random_name: String = String::from(NAMES[rng.gen_range(0, NAMES.len())]);
+            let random_surname: String = String::from(SURNAMES[rng.gen_range(0, SURNAMES.len())]);
+
+            students.push(Student {
+                name: format!("{} {}", random_name, random_surname),
+                class_id: String::from(rng.gen_range(1, 5).to_string()),
+                marks: vec![
+                    // shitty solution cuz idk how to do that correctly in Rust, sorry xD
+                    rng.gen_range(1, 6),
+                    rng.gen_range(1, 6),
+                    rng.gen_range(1, 6),
+                    rng.gen_range(1, 6),
+                    rng.gen_range(1, 6),
+                    rng.gen_range(1, 6),
+                    rng.gen_range(1, 6),
+                ],
+            })
+        }
+        return students;
+    }
+
+    fn print_all_students(&self) {
+        for student in self.students.iter() {
+            student.pretty_print();
+        }
+    }
+}
+
+fn main() {
+    let mut school: School = School::new(String::from("xD"), vec![]);
+
+    school.students = school.generate_students(12);
+    school.print_all_students();
+}
